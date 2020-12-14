6a6a9619cace7bcc51ee30ff3e8d7006bf764325
oysandvik94 <oysandvik94@gmail.com>
2020-12-14 17:14:09 +0100

    Removed most of the template/example code

---
+++ webapp/Controllers/OrganizationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace webapp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrganizationController : ControllerBase
+    {
+        private readonly ILogger<OrganizationController> _logger;
+
+        public OrganizationController(ILogger<OrganizationController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
